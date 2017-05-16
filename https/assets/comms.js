ea25ebc6f27832d235411cb791197b7f93939e40
lucyrose93 <lucyrose@protonmail.com>
2017-05-16 16:58:36 +0100

    progress from Tues

--- https/assets/comms.js
+++ https/assets/comms.js
@@ -8,15 +8,19 @@ const EndPoint =
    *  The signaling transport layer *must* be encapsulated in this class such that replacing one transport
    *  (eg REST Polling) with another (eg WebSockets) should not require any changes to the derived classes.
    */
+
   class EndPoint {
     constructor(ep_name) {
+      this._name = ep_name;
+      EndPoint.names[ep_name] = this;
     }
     /** @method log
      *  @description simple wrapper around console.log that prefixes the name ofthe EndPoint that's generating the message
      */
-     log(...args) {
-       console.log('NAME: '+this._name,...args);
-     }
+    log(...args) {
+      console.log('NAME: '+this._name,...args);
+    }
+
     /** @method send
      *  @description Send a message to the named target EndPoint (which is usually on a remote client)
      *  @param {String} targetName - the unique name of the end point
@@ -24,6 +28,7 @@ const EndPoint =
      *  @param {Object} [data] - optional parameter data to send with this message
      */
     send(targetName, operation, data) {
+      EndPoint.names[targetName].receive(this._name, operation, data);
     }
     /** @method receive
      *  @description This method will return a message from a remote end point. This method *MUST* be overridden in the
@@ -33,5 +38,8 @@ const EndPoint =
       console.error("Virtual base class method 'receive' called - this should always be overridden in a derived class");
     }
   }
+
+  EndPoint.names = {};
+
   return EndPoint;
 })();
--- https/assets/driver.js
+++ https/assets/driver.js
@@ -2,5 +2,11 @@
 (function() {
   document.addEventListener("DOMContentLoaded", function() {
     // Application logic here
+    const V1 = new VideoEndPoint('V1');
+    const V2 = new VideoEndPoint('V2');
+    debugger;
+
+    V1.send('V2', 'SDP_ANSWER', {a: 'yo'});
+
   });
 })();
